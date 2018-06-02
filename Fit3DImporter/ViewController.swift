66c73fee6fae557187fbba780aff5c1b48143100
John Szumski <jszumski@gmail.com>
2018-06-01 22:10:37 -0400

    Initial Commit

---
+++ Fit3DImporter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fit3DImporter
+//
+//  Created by John Szumski on 6/1/18.
+//  Copyright © 2018 John Szumski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
